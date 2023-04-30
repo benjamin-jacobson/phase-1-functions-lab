08cd8b1216ed11c1edbd395282a0087f4c07b545
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-04-30 16:42:31 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block){
+    //console.log(Math.abs(42 - block))
+    return Math.abs(42 - block)
+}
+
+function distanceFromHqInFeet(block){
+    let instanceBlocks = distanceFromHqInBlocks(block)
+    console.log(instanceBlocks * 264)
+    return instanceBlocks * 264
+}
+
+distanceFromHqInBlocks(40)
+distanceFromHqInFeet(10)
+
+function distanceTravelledInFeet(startBlock,endBlock){
+    return 264 * Math.abs(startBlock - endBlock )
+}
+
+function calculatesFarePrice(start, destination){
+    let totalDistance = distanceTravelledInFeet(start, destination)
+    // Conditional
+    if (totalDistance < 400 ) {
+        return 0;
+    } else if (totalDistance >= 400 && totalDistance <= 2000){
+        return 2*(totalDistance - 400)/100;
+    } else if (totalDistance > 2000 && totalDistance <= 2500) {
+        return 25;
+    } else if (totalDistance > 2500) {
+    return 'cannot travel that far';
+}
+}
